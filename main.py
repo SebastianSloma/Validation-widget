644cd37a24d38a9ce994d99d8298f31ca78ede2c
Sebastian <sebastiansloma@gmail.com>
2022-12-13 05:57:55 +0100

    created a place to enter login and password

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import tkinter
-
+# import library tkinter
 
 window = tkinter.Tk()
 window.title('Login from')
 window.geometry('400x500')
-window.configure(bg='#333333')
+window.configure(bg='#808080')
 
+# Create widgets
+login_label = tkinter.Label(window, text='Login')
+username_label = tkinter.Label(window, text='Username')
+username_entry = tkinter.Entry(window)
+password_label = tkinter.Label(window, text='Password')
+password_entry = tkinter.Entry(window, show='*')
+login_button = tkinter.Button(window, text='Login')
+
+# widget placement on the screen
+login_label.grid(row=0,column=0,columnspan=2)
+username_label.grid(row=1, column=0)
+username_entry.grid(row=1,column=1)
+password_label.grid(row=2, column=0)
+password_entry.grid(row=2,column=1)
+login_button.grid(row=3,column=0, columnspan=2)
 window.mainloop()
